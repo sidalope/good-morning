8a7d0459ac1841e3b5c70757a5d46bfc8a84a6d0
alessandrobisiani <bisi90@gmail.com>
2019-04-19 13:35:17 -0400

    feat: Add successful sms logging with print
    TODO: add sms log saving to file

--- send_text.py
+++ send_text.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python3
 from twilio.rest import Client
 from sys import argv
 from os import getenv
@@ -17,13 +17,32 @@ sender = getenv('TWILIO_NUMBER')
 class Messenger:
 
     def send_message(self, recipient, body):
+        '''
+        Send an sms "body" to "recipient", and log the sid on a
+        successful send
+
+        Returns:
+        {str} : the message sid
+        '''
         client = Client(account_sid, auth_token)
         message = client.messages.create(
                 to=recipient,
                 from_=sender,
                 body=body)
 
+        self.log_sms(message.sid, recipient, body)
         return message.sid
+
+
+    def log_sms(self, sid, recipient, body):
+        '''
+        Log an sms by printing to stdout
+
+        TODO: Append to a file. Add file path in args for github.
+        '''
+        print(f'sid: {sid}')
+        print(f'Sent to: {recipient}')
+        print(f'Body: {body}\r\n')
 
 
 if __name__ == '__main__':
@@ -31,7 +50,6 @@ if __name__ == '__main__':
     if len(argv) == 3:
         recipient = argv[1]
         body = argv[2]
-        # print(f'from: {sender} to: {receiver} message: {body}')
 
         Messenger().send_message(recipient, body)
     else:
