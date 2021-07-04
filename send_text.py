12c00f7c313ba5cb7b232f6206088de3dc865f04
Alessandro Bisiani <alessandro.bisiani.1@gmail.com>
2021-07-05 00:09:44 +0200

    Add logger and requirements file
    Tested the API. Basic sms sending is working well at the moment.
    
    Using a US numnber at the moment. TODO: Implement testing, the
    cron job, and find a cheaper option.

--- send_text.py
+++ send_text.py
@@ -1,37 +1,52 @@
 #!/usr/bin/env python3
-from twilio.rest import Client
 from sys import argv
 from os import getenv
+import logging
+from twilio.rest import Client
 from dotenv import load_dotenv
-from pathlib import Path
 
-environment_path = Path('.') / '.env'
-load_dotenv(dotenv_path=environment_path)
-
-# Read Account SID and Auth Token (twilio.com/console)
-account_sid = getenv('TWILIO_ACCOUNT_SID')
-auth_token = getenv('TWILIO_AUTH_TOKEN')
-sender = getenv('TWILIO_NUMBER')
+load_dotenv()
 
 
 class Messenger:
+    account_sid = getenv('TWILIO_ACCOUNT_SID')
+    auth_token = getenv('TWILIO_AUTH_TOKEN')
+    sender = getenv('TWILIO_NUMBER')
 
-    def send_message(self, recipient, body):
+    test_account_sid = getenv('TEST_ACCOUNT_SID')
+    test_auth_token = getenv('TEST_AUTH_TOKEN')
+    test_sender = getenv('TEST_NUMBER')
+    test_recipient = getenv('TEST_RECIPIENT')
+
+    client = Client(test_account_sid, test_auth_token)
+
+    # Define logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    fh = logging.FileHandler('./log.txt')
+    fh.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    client.http_client.logger.setLevel(logging.INFO)
+
+
+    def send_message(self, body, recipient=test_recipient):
         '''
         Send an sms "body" to "recipient", and log the sid on a
         successful send
 
         Returns:
-        {str} : the message sid
+        {str} : the message delivery status
         '''
-        client = Client(account_sid, auth_token)
-        message = client.messages.create(
-                to=recipient,
-                from_=sender,
-                body=body)
+        # print(self.test_sender)
+        message = self.client.messages.create(
+                body = body,
+                from_ = self.test_sender,
+                to = recipient)
 
-        self.log_sms(message.sid, recipient, body)
-        return message.sid
+        return message.status
 
 
     def log_sms(self, sid, recipient, body):
@@ -50,8 +65,12 @@ if __name__ == '__main__':
     if len(argv) == 3:
         recipient = argv[1]
         body = argv[2]
-
-        Messenger().send_message(recipient, body)
+        response = Messenger().send_message(body, recipient)
+        print(response.sid)
+    elif len(argv) == 2:
+        body = argv[1]
+        response = Messenger().send_message(body)
+        print(response)
     else:
-        print(f'You passed in {len(argv)} arguments.')
+        print(f'You passed in {len(argv) - 1} arguments.')
         print('send_text takes 2 arguments: a recipient and a message body.')
